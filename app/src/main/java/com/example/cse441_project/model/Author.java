0b8390dac5fbcd407bd4ff69f6404bf329c5c7f4
Tuan Dat <datto25082003@gmail.com>
2024-10-26 22:09:19 +0700

    Store code

---
+++ app/src/main/java/com/example/cse441_project/model/Author.java
@@ -0,0 +1,10 @@
+package com.example.cse441_project.model;
+import lombok.*;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author {
+    private String id;
+    private String name;
+}
---
+++ app/src/main/java/com/example/cse441_project/model/Category.java
@@ -0,0 +1,12 @@
+package com.example.cse441_project.model;
+import lombok.*;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    private String id;
+    private String name;
+
+
+}
