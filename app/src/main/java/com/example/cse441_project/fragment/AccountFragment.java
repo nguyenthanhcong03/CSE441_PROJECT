d29190363e2b7d402d74854ca225715c7863db93
Huongne1001 <huongne1001@gmail.com>
2024-10-28 10:16:07 +0700

    store code

--- app/src/main/java/com/example/cse441_project/fragment/AccountFragment.java
+++ app/src/main/java/com/example/cse441_project/fragment/AccountFragment.java
@@ -1,13 +1,16 @@
 package com.example.cse441_project.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.cse441_project.R;
 
@@ -15,6 +18,18 @@ public class AccountFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_manage_account, container, false);
+        View view = inflater.inflate(R.layout.fragment_manage_account, container, false);
+        Button btnEditAccount = view.findViewById(R.id.btnEditAccount);
+
+
+        btnEditAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), EditAdminProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
--- app/src/main/java/com/example/cse441_project/fragment/EditAdminProfileActivity.java
+++ app/src/main/java/com/example/cse441_project/fragment/EditAdminProfileActivity.java
@@ -1,4 +1,4 @@
-package com.example.cse441_project.QuanLy;
+package com.example.cse441_project.fragment;
 
 import android.os.Bundle;
 
@@ -10,13 +10,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.cse441_project.R;
 
-public class ThongTinCaNhan extends AppCompatActivity {
+public class EditAdminProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.fragment_huong_ttcanhan);
+        setContentView(R.layout.activity_edit_admin_profile);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
